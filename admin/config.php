ff954220df9f3a945d3ff8a294e067266bed0bc2
Klaitos <chris.klaitos@gmail.com>
2013-07-19 01:14:56 +0200

    fix var & wamp

--- admin/config.php
+++ admin/config.php
@@ -1,29 +1,29 @@
 <?php
 
-	require 'vendor/autoload.php';
-	require_once('tools/fonctions.php');
-	$path = __DIR__."/includes/";
-	set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+require 'vendor/autoload.php';
+require_once('tools/fonctions.php');
+$path = __DIR__."/includes/";
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 
-	define('BRAND', 'Erasme');
-	define('BRAND_CLEAN', strtolower(preg_replace("/[^a-z]+/i", "-", BRAND)));
+define('BRAND', 'Erasme');
+define('BRAND_CLEAN', strtolower(preg_replace("/[^a-z]+/i", "-", BRAND)));
 
 
-if(preg_match('/wamp/', $_SERVER['DOCUMENT_ROOT'])) {
-	define('DEBUG', true);
-	define('BASE_URL', '/museo/admin/');
-	define('DB_NAME', 'museotouch');
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', '');
-	define('DB', '127.0.0.1');
-}
+
+define('DEBUG', true);
+define('BASE_URL', '/museo/admin/');
+define('DB_NAME', 'museotouch');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB', '127.0.0.1');
+
 
 define('DIR_PHOTO', BASE_URL."assets/img/");
 define('DIR_PHOTO_OBJETS', DIR_PHOTO."objets/");
 define('DIR_PHOTO_EXPOS', DIR_PHOTO."expositions/");
 
-	session_start();
-	$uri = URI();
+session_start();
+$uri = URI();
 
 
 	if($_SERVER["REQUEST_URI"] != BASE_URL) //Toutes pages sauf l'accueil
@@ -38,81 +38,80 @@ define('DIR_PHOTO_EXPOS', DIR_PHOTO."expositions/");
 
 
 
-	/* Configuration générales du BO */
+		/* Configuration générales du BO */
 
-	$menu_links = array(
-		$link1 = array(
+		$menu_links = array(
+			$link1 = array(
 				"nom" => 'Exposition',
 				"href" => "exposition",
 				"level_required" => 0,
 				"active" => 'exposition' ),
-		$link2 = array(
+			$link2 = array(
 				"nom" => 'Administration',
 				"href" => "administration",
 				"level_required" => 0,
 				"active" => 'administration' ),
-		$link3 = array(
+			$link3 = array(
 				"nom" => 'Musee',
 				"href" => "musee",
 				"level_required" => 0,
 				"active" => 'musee'),
-		$link4 = array(
+			$link4 = array(
 				"nom" => 'Front Office',
 				"href" => "../front",
 				"level_required" => 0,
 				"active" => 'front' )
-				);		
-				
+			);
 
-	$menu_links_administration = array(
-		$link1 = array(
+		$menu_links_administration = array(
+			$link1 = array(
 				"nom" => 'Utilisateurs',
 				"href" => "administration/users",
 				"level_required" => 0,
 				"active" => 'user-liste' ),
-		$link2 = array(
+			$link2 = array(
 				"nom" => 'Droits',
 				"href" => "administration/droits",
 				"level_required" => 0,
 				"active" => 'droit-liste' ),
-	);
+			);
 
-	$menu_links_musee = array(
-		$link1 = array(
+		$menu_links_musee = array(
+			$link1 = array(
 				"nom" => 'Musees',
 				"href" => "musee/musee-liste",
 				"level_required" => 0,
-				"active" => 'musee-liste') 
-	);
+				"active" => 'musee-liste')
+			);
 
-	$menu_links_exposition = array(
-		$link1 = array(
+		$menu_links_exposition = array(
+			$link1 = array(
 				"nom" => 'Expositions',
 				"href" => "exposition/exposition-liste",
 				"level_required" => 0,
 				"active" => 'exposition-liste' ),
-		$link2 = array(
+			$link2 = array(
 				"nom" => 'Objets',
 				"href" => "exposition/objet-liste",
 				"level_required" => 0,
 				"active" => 'objet-liste' ),
-	);
+			);
 
 
 
 
-	$active = "";
-	$html = "";
+		$active = "";
+		$html = "";
 
-	$_POST = array_map('strip_tags', $_POST);
-	$_GET = array_map('strip_tags', $_GET);
-	$_POST = array_map('stripslashes', $_POST);
-	$_GET = array_map('stripslashes', $_GET);
+		$_POST = array_map('strip_tags', $_POST);
+		$_GET = array_map('strip_tags', $_GET);
+		$_POST = array_map('stripslashes', $_POST);
+		$_GET = array_map('stripslashes', $_GET);
 
 	//Pour se deconnecter
-	if(@$_GET['action'] == "deco"){
-		session_destroy();
-		header("Location: ".BASE_URL);
-	}
+		if(@$_GET['action'] == "deco"){
+			session_destroy();
+			header("Location: ".BASE_URL);
+		}
 
-	$db = connect();
+		$db = connect();
--- api/config.php
+++ api/config.php
@@ -3,14 +3,13 @@
 require 'vendor/autoload.php';
 require_once('tools/fonctions.php');
 
-if(preg_match('/wamp/', $_SERVER['DOCUMENT_ROOT'])) {
-	define('DEBUG', true);
-	define('BASE_URL', '/museo/admin/');
-	define('DB_NAME', 'museotouch');
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', '');
-	define('DB', '127.0.0.1');
-}
+define('DEBUG', true);
+define('BASE_URL', '/museo/admin/');
+define('DB_NAME', 'museotouch');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB', '127.0.0.1');
+
 
 define('DIR_PHOTO', '/assets/img/objets/');
 define('DIR_PHOTO_OBJETS', DIR_PHOTO.'objets/');
@@ -19,10 +18,10 @@ $db = connect(); // http://www.notorm.com/ beaucoup d'example sur : http://sql-c
 
 \Slim\Route::setDefaultConditions(array(
 	'id' => '[0-9]{1,}'
-));
+	));
 
 $app = new \Slim\Slim(array(
-    'debug' => true
-));
+	'debug' => true
+	));
 
 $app->contentType('text/html; charset=utf-8');
